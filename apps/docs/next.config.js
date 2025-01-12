da028fa8b3f28867726cea9fd1d6a0cb73551a36
Jamie Nichols <jamienichols@codingbutter.com>
2025-01-12 13:27:27 +0000

    Initial Commit with Docs

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+import createMDX from "@next/mdx"
+
+const nextConfig = {
+  // Configure `pageExtensions` to include markdown and MDX files
+  // Optionally, add any other Next.js config below
   reactStrictMode: true,
   transpilePackages: ["@repo/ui"],
-};
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+}
+
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+})
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
